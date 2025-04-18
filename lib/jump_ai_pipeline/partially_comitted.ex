9b81fb129edac41bfb06e8fadc7204147260b834
Constantine Manakov <konstantinmanakow@gmail.com>
2025-04-18 20:28:51 +0200

    test pipeline

--- lib/jump_ai_pipeline/partially_comitted.ex
+++ lib/jump_ai_pipeline/partially_comitted.ex
@@ -2,4 +2,9 @@ defmodule JumpAiPipeline.PartiallyComitted do
   def old_function(a, b) do
     Enum.count(a) + Enum.count(b)
   end
+
+  # useless commentary
+  def another_function(a, b) do
+    a ++ b
+  end
 end
