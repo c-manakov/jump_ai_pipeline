50dce53d7ef72d35a488f950e9ecb2c51644e34f
Constantine Manakov <konstantinmanakow@gmail.com>
2025-04-18 15:26:53 +0200

    remove test files

--- lib/jump_ai_pipeline/partially_comitted.ex
+++ lib/jump_ai_pipeline/partially_comitted.ex
@@ -1,11 +1,5 @@
 defmodule JumpAiPipeline.PartiallyComitted do
-  # this shouldn't trigger a generated test
   def old_function(a, b) do
     Enum.count(a) + Enum.count(b)
-  end
-
-  # this should
-  def new_function(a, b) do
-    a ++ b
   end
 end
